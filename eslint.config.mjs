48798a790d7a1e08574e6b333a121d411a50bc56
Daniel Silva <dfsilva.dxp@gmail.com>
2026-03-09 15:33:02 -0300

    chore(eslint): configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "src/generated/**",
+      "build/**",
+      "coverage/**",
+      "*.config.js",
+    ],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { prettier: prettierPlugin },
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended[0].rules,
+
+      "no-console": "error",
+      "no-unused-vars": [
+        "error",
+        { args: "after-used", argsIgnorePattern: "^_" },
+      ],
+      "prettier/prettier": "warn",
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+  prettierConfig,
+]);
