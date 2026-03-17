269926ecc7eff035cb23af4ab9f4c13899c9e3a0
Daniel Silva <dfsilva.dxp@gmail.com>
2026-03-17 09:44:16 -0300

    refactor: remove application DTO dependency from domain layer

--- src/domain/repositories/account.repository.ts
+++ src/domain/repositories/account.repository.ts
@@ -1,7 +1,13 @@
-import { type ISignUpDTO } from "../../application/dtos/account/sign-up.dto";
-import { type Account } from "../../generated/prisma/client";
+import { type Account, type Role } from "../../generated/prisma/client";
+
+export interface IAccountCreateDTO {
+  readonly name: string;
+  readonly email: string;
+  readonly password: string;
+  readonly role: Role;
+}
 
 export interface IAccountRepository {
   readonly findByEmail: (_email: string) => Promise<Account | null>;
-  readonly create: (_input: ISignUpDTO) => Promise<Account>;
+  readonly create: (_input: IAccountCreateDTO) => Promise<Account>;
 }
